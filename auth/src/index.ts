de77869c2e4e6cb628f96d8d3037a95c50bd5315
Harish <r.shivi@yahoo.com>
2022-06-05 17:53:04 +0530

    feat: add k8s setup

---
+++ auth/src/index.ts
@@ -0,0 +1,19 @@
+import { json } from 'body-parser'
+import express from 'express'
+
+import { currentUserRouter } from './routes/currentUser'
+import { signInRouter } from './routes/signin'
+import { signOutRouter } from './routes/signout'
+import { signUpRouter } from './routes/signup'
+
+const app = express()
+app.use(json())
+
+app.use(currentUserRouter)
+app.use(signInRouter)
+app.use(signOutRouter)
+app.use(signUpRouter)
+
+app.listen(3000, () => {
+  console.log('Listening on port 3000!!')
+})
