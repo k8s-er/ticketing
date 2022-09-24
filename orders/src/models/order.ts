b8d8c3c2c986d14dceef261f2bcb700ca89a560e
Harish <r.shivi@yahoo.com>
2022-09-24 15:36:53 +0530

    fix: orders model

--- orders/src/models/order.ts
+++ orders/src/models/order.ts
@@ -1,15 +1,18 @@
+import { OrderStatus } from '@hrdev/common';
 import mongoose from 'mongoose';
+
+import { TicketDoc } from './ticket';
 
 interface OrderAttrs {
   userId: string;
-  status: string;
+  status: OrderStatus;
   expiresAt: Date;
   ticket: TicketDoc;
 }
 
 interface OrderDoc extends mongoose.Document {
   userId: string;
-  status: string;
+  status: OrderStatus;
   expiresAt: Date;
   ticket: TicketDoc;
 }
@@ -27,6 +30,8 @@ const orderSchema = new mongoose.Schema(
     status: {
       type: String,
       required: true,
+      enum: Object.values(OrderStatus),
+      default: OrderStatus.Created,
     },
     expiresAt: {
       type: mongoose.Schema.Types.Date,
